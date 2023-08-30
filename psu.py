3a3ad715e90f3613dbc1dd5e2bab8d1f242f65c9
Budi Sendjaja <budi.sendjaja@abbott.com>
2023-08-30 13:23:19 -0500

    Add random toggle

--- psu.py
+++ psu.py
@@ -2,6 +2,15 @@
 import pyvisa
 import time
 import sys
+import random
+import signal
+
+def signal_handler(sig, frame):
+    print("Ctrl+C is pressed, turning off PSU")
+    psu.write("OUTPUT OFF")
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
 
 # visa.log_to_screen()
 rm = pyvisa.ResourceManager('@py')
@@ -52,9 +61,11 @@ print(psu.query('*IDN?'))
 
 state = psu.query("OUTPUT:STATE?")
 
-magnet = 1
+magnet = 0
+random_enable = 1
+run_number = -1
 
-if magnet == 1:
+if magnet == 1 and random_enable == 0:
     psu.write(":OUTP OFF")
     psu.write(":VOLT 12")
     psu.write(":CURR 1")
@@ -82,7 +93,29 @@ if magnet == 1:
     psu.write(":OUTP OFF")
 
     print("Magnet OFF")
-else:
+elif magnet == 0 and random_enable == 1:
+    print("Random")
+    iteration = 0
+
+    # print("rng = " + str(random.randrange(0, 5, 0.2)))
+    #values = range(6)
+    #for x in values:
+    psu.write(":OUTP OFF")
+    psu.write(":VOLT 4.0")
+    psu.write(":CURR 0.1")
+    while run_number == -1:
+        iteration += 1
+        psu.write("OUTPUT ON")
+        a = random.randint(200, 500)
+        f = a/1000
+        time.sleep(f)
+        psu.write("OUTPUT OFF")
+        b = random.randint(1, 6)
+
+        time.sleep(b)
+
+        print(str(iteration).zfill(4) + " : button press of " + "{:1.3f}".format(f) + " milliseconds and delay of " +str(b) + " seconds")
+elif magnet == 0 and random_enable == 0:
     psu.write(":OUTP OFF")
     psu.write(":VOLT 3.8")
     psu.write(":CURR 1")
@@ -92,5 +125,6 @@ else:
     else:
         print("OFF")
         psu.write("OUTPUT OFF")
-
+else:
+    print("Cannot have both magnet and random = 1")
     sys.exit(0)
